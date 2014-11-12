e6846358e780c711341e276598a4ec3c543c100e
Daniel Santos <dsantos@cryptolab.net>
2014-11-12 17:56:39 +0000

    override hosts file

--- docker/recipes/pull_images.rb
+++ docker/recipes/pull_images.rb
@@ -1,18 +1,21 @@
 # Pull each of our defined apps
-node[:my_docker].each do |name, image|  
-  script "pull_app_#{name}_image" do
+#node[:my_docker].each do |name, image|  
+#  script "pull_app_#{name}_image" do
+#    interpreter "bash"
+#    user "root"
+#    code <<-EOH
+#      docker pull #{image}
+#    EOH
+#  end
+#end  
+# Pull latest Nginx
+node[:app][:dockers].each do |name, image|
+  script "pull_images" do  
     interpreter "bash"
     user "root"
     code <<-EOH
-      docker pull #{image}
+      DOMAIN="#{node[:app][:domain]}"
+      docker pull ${DOMAIN}/#{image}
     EOH
   end
-end  
-# Pull latest Nginx
-script "pull_nginx_image" do  
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-    docker pull #{node[:my_nginx]}
-  EOH
-end  
+end
--- docker/recipes/run_containers.rb
+++ docker/recipes/run_containers.rb
@@ -1,25 +1,36 @@
-# Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
-node[:docker].each do |name, image|  
-  script "run_app_#{name}_container" do
+## Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
+#node[:docker].each do |name, image|  
+#  script "run_app_#{name}_container" do
+#    interpreter "bash"
+#    user "root"
+#    code <<-EOH
+#      docker run -d --name=#{name} #{image}
+#    EOH
+#  end
+#end
+#
+## Run Nginx, linking it to all the other Apache containers, and expose its port.
+#script "run_nginx_container" do  
+#  links = ''
+#  node[:docker].keys.each do |name|
+#    # prefixed with "app_" here for ease of discovery
+#    # see: http://jaredmarkell.com/nginx-proxy-through-linked-docker-containers/
+#    links += " --link=#{name}:app_#{name}"
+#  end
+#  interpreter "bash"
+#  user "root"
+#  code <<-EOH
+#    docker run -d -p 80:80 --name=nginx #{links} #{node[:my_nginx]}
+#  EOH
+#end  
+
+node[:app][:dockers].each do |name, image|
+  script "run_#{name}_container" do
     interpreter "bash"
     user "root"
     code <<-EOH
-      docker run -d --name=#{name} #{image}
+      DOMAIN="#{node[:app][:domain]}"
+      docker run -d -p 80:80 --name=#{name} ${DOMAIN}/#{image}
     EOH
   end
 end
-
-# Run Nginx, linking it to all the other Apache containers, and expose its port.
-script "run_nginx_container" do  
-  links = ''
-  node[:docker].keys.each do |name|
-    # prefixed with "app_" here for ease of discovery
-    # see: http://jaredmarkell.com/nginx-proxy-through-linked-docker-containers/
-    links += " --link=#{name}:app_#{name}"
-  end
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-    docker run -d -p 80:80 --name=nginx #{links} #{node[:my_nginx]}
-  EOH
-end  
