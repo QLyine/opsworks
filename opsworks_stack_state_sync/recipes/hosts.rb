6ab89ff58d5d9dc3661bf818e8a415ae5e259a35
Daniel Santos <dsantos@cryptolab.net>
2014-11-12 15:27:10 +0000

    override hosts file

--- opsworks_stack_state_sync/recipes/hosts.rb
+++ opsworks_stack_state_sync/recipes/hosts.rb
@@ -4,9 +4,9 @@ template '/etc/hosts' do
   source "hosts.erb"
   mode "0644"
   variables(
-    :localhost_name  => node[:opsworks][:instance][:hostname],
+    :localhost_name => node[:opsworks][:instance][:hostname],
     :registry_domain => node[:app][:domain],
-    :registry_ip     => node[:app][:ip],
+    :registry_ip => node[:app][:ip],
     :nodes => search(:node, "name:*")
   )
 end
