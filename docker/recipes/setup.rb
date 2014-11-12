a617681199c1170a7adec55a4fb85798dc84c177
Daniel Santos <dsantos@cryptolab.net>
2014-11-12 14:08:42 +0000

    add cert script

--- docker/recipes/setup.rb
+++ docker/recipes/setup.rb
@@ -1,4 +1,16 @@
-package "docker" do  
-    package_name "lxc-docker-0.9.0"
-    action :install
-end  
+script "install_certs" do
+  interpreter "bash" 
+  user "root"
+  code <<-EOH
+  DIR="/etc/docker/certs.d/registry.aptoide.com";
+  ODIR="/usr/local/share/ca-certificates/registry.aptoide.com";
+  FILE="devdockerCA.crt";
+  USER="#{node[:app][:username]}";
+  USER="#{node[:app][:password]}";
+  [[ ! -d $DIR ]] && mkdir -p $DIR ;
+  [[ ! -d $ODIR ]] && mkdir -p $ODIR ;
+  wget -O ${DIR} http://${USER}:${PASS}@registry.aptoide.com/${FILE};
+  wget -O ${ODIR} http://${USER}:${PASS}@registry.aptoide.com/${FILE};
+  update-ca-certificates;
+  EOH
+end
