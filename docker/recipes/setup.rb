c2fd16026dc133392ceb32de178a7a1acfb061f5
Daniel Santos <dsantos@cryptolab.net>
2014-11-12 14:28:18 +0000

    add cert script

--- docker/recipes/setup.rb
+++ docker/recipes/setup.rb
@@ -2,15 +2,18 @@ script "install_certs" do
   interpreter "bash" 
   user "root"
   code <<-EOH
-  DIR="/etc/docker/certs.d/registry.aptoide.com";
-  ODIR="/usr/local/share/ca-certificates/registry.aptoide.com";
+  DOMAIN="#{node[:app][:domain]}";
+  IP="#{node[:app][:ip]}";
+  DIR="/etc/docker/certs.d/${DOMAIN}";
+  ODIR="/usr/local/share/ca-certificates/${DOMAIN}";
   FILE="devdockerCA.crt";
   USER="#{node[:app][:username]}";
   USER="#{node[:app][:password]}";
+  echo "${IP}  ${DOMAIN} " >> /etc/hosts ;
   [[ ! -d $DIR ]] && mkdir -p $DIR ;
   [[ ! -d $ODIR ]] && mkdir -p $ODIR ;
-  wget -O ${DIR} http://${USER}:${PASS}@registry.aptoide.com/${FILE};
-  wget -O ${ODIR} http://${USER}:${PASS}@registry.aptoide.com/${FILE};
+  wget -O ${DIR} http://${USER}:${PASS}@${DOMAIN}/${FILE};
+  wget -O ${ODIR} http://${USER}:${PASS}@${DOMAIN}/${FILE};
   update-ca-certificates;
   EOH
 end
