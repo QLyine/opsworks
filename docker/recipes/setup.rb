b254d9c6bc82054c138eba2ba924cbe8a9bfd054
Daniel Santos <dsantos@cryptolab.net>
2014-11-12 16:58:00 +0000

    override hosts file

--- docker/recipes/setup.rb
+++ docker/recipes/setup.rb
@@ -3,17 +3,15 @@ script "install_certs" do
   user "root"
   code <<-EOH
   DOMAIN="#{node[:app][:domain]}";
-  IP="#{node[:app][:ip]}";
   DIR="/etc/docker/certs.d/${DOMAIN}";
   ODIR="/usr/local/share/ca-certificates/${DOMAIN}";
   FILE="devdockerCA.crt";
   USER="#{node[:app][:username]}";
-  USER="#{node[:app][:password]}";
-  echo "${IP}  ${DOMAIN} " >> /etc/hosts ;
+  PASS="#{node[:app][:password]}";
   [[ ! -d $DIR ]] && mkdir -p $DIR ;
   [[ ! -d $ODIR ]] && mkdir -p $ODIR ;
-  wget -O ${DIR}/${FILE}  http://${USER}:${PASS}@${DOMAIN}/${FILE};
-  wget -O ${ODIR}/${FILE} http://${USER}:${PASS}@${DOMAIN}/${FILE};
+  wget -O ${DIR}/${FILE}  "http://${USER}:${PASS}@${DOMAIN}/${FILE}";
+  wget -O ${ODIR}/${FILE} "http://${USER}:${PASS}@${DOMAIN}/${FILE}";
   update-ca-certificates;
   EOH
 end
