a2ef293065254c8d1e7a967108fc60a4eb49a09c
Daniel Santos <dsantos@cryptolab.net>
2014-11-13 11:54:55 +0000

    override hosts file

--- docker/recipes/pull_images.rb
+++ docker/recipes/pull_images.rb
@@ -9,18 +9,17 @@
 #  end
 #end  
 
-
-
-#script "login_to_registry" do
-#  interpreter "bash" 
-#  user "root"
-#  code <<-EOH
-#  DOMAIN="#{node[:app][:domain]}";
-#  USER="#{node[:app][:username]}";
-#  PASS="#{node[:app][:password]}";
-#  docker login --username="${USER}" --password="${PASS}" --email="" ${DOMAIN} 
-#  EOH
-#end
+script "login_to_registry" do
+  interpreter "bash" 
+  user "root"
+  code <<-EOH
+  DOMAIN="#{node[:app][:domain]}";
+  USER="#{node[:app][:username]}";
+  PASS="#{node[:app][:password]}";
+  MAIL="NONE";
+  docker login --username="${USER}" --password="${PASS}" --email="$[MAIL}" ${DOMAIN} ;
+  EOH
+end
 
 # Pull latest Nginx
 node[:app][:dockers].each do |name, image|
