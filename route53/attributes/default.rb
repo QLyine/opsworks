53e4641b14811708622e980f93063ef790b195f8
Daniel Santos <dsantos@cryptolab.net>
2014-11-21 10:01:32 +0000

    add dnsupdate

--- route53/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['build_essential']['compiletime'] = true
-default['build-essential']['compile_time'] = true
-default['route53']['nokogiri_version'] = '1.6.3.1'
-default['route53']['fog_version'] = '1.24'
