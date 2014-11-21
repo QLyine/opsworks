d7acf0cc96271872a363c326dd7b87607f1dc945
Daniel Santos <dsantos@cryptolab.net>
2014-11-21 17:34:48 +0000

    add dnsupdate

--- route53-registry/recipes/default.rb
+++ route53-registry/recipes/default.rb
@@ -1,8 +1,8 @@
-client = Route53::Client.new(node[:route53_registry][:aws_access_key_id], node[:route53_registry][:aws_secret_access_key])
+client = Route53::Client.new(node[:app][:custom_access_key], node[:app][:custom_secret_key])
 
-name = node[:route53_registry][:name]
-value = node[:opsworks][:instance][:private_ip]
+name = "www.test.aptoide.com"
+value = node[:opsworks][:instance][:public_ip]
 type = "A"
-zone_id = node[:route53_registry][:zone_id]
+zone_id = node[:app][:dns_zone_id]
 
 client.create_or_append_record(name, value, type, 60, zone_id)
