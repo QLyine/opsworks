9c6c129c215d9dfbbd3d63044663f7450a7a6892
Daniel Santos <dsantos@cryptolab.net>
2014-12-05 16:05:00 +0000

    added generic sysctl

--- server/attributes/default.rb
+++ server/attributes/default.rb
@@ -1,21 +1,25 @@
-default[:redis][:sysctl] = Mash.new
-default[:redis][:sysctl]['vm.swappiness']=0
-default[:redis][:sysctl]['net.ipv4.tcp_sack']=1
-default[:redis][:sysctl]['net.ipv4.tcp_timestamps']=1
-default[:redis][:sysctl]['net.ipv4.tcp_window_scaling']=1
-default[:redis][:sysctl]['net.ipv4.tcp_congestion_control']='htcp'
-default[:redis][:sysctl]['net.ipv4.tcp_syncookies']=1
-default[:redis][:sysctl]['net.ipv4.tcp_tw_recycle']=1
-default[:redis][:sysctl]['net.ipv4.tcp_slow_start_after_idle']=0
-default[:redis][:sysctl]['net.ipv4.ip_no_pmtu_disc']=1
-default[:redis][:sysctl]['net.ipv4.tcp_sack']=1
-default[:redis][:sysctl]['net.ipv4.tcp_timestamps']=1
+default[:generic][:sysctl] = Mash.new
+# SWAP 
+default[:generic][:sysctl]['vm.swappiness']=0
+# TCP
+default[:generic][:sysctl]['net.ipv4.tcp_congestion_control']='htcp'
+default[:generic][:sysctl]['net.core.rmem_max']=16777216
+default[:generic][:sysctl]['net.core.wmem_max']=16777216
 
-default[:redis][:sysctl]['net.core.rmem_max']=16777216
-default[:redis][:sysctl]['net.core.wmem_max']=16777216
 
-default[:redis][:sysctl]['net.ipv4.tcp_rmem']='"4096 524288 16777216"'
-default[:redis][:sysctl]['net.ipv4.tcp_wmem']='"4096 524288 16777216"'
+# Set minimum, default, and maximum TCP buffer limits
+default[:generic][:sysctl]['net.ipv4.tcp_rmem']='"4096 524288 16777216"'
+default[:generic][:sysctl]['net.ipv4.tcp_wmem']='"4096 524288 16777216"'
 
+# Set maximum network input buffer queue length
+default[:generic][:sysctl]['net.core.netdev_max_backlog']=250000
+
+default[:generic][:sysctl]['net.ipv4.ip_local_port_range']='"1024 65000"'
+
+default[:generic][:sysctl]['net.ipv4.tcp_slow_start_after_idle']=0
+default[:generic][:sysctl]['net.ipv4.ip_no_pmtu_disc']=1
+default[:generic][:sysctl]['net.ipv4.tcp_timestamps']=1
+default[:generic][:sysctl]['net.ipv4.tcp_syncookies']=1
+default[:generic][:sysctl]['net.ipv4.tcp_tw_reuse']=1
 
 
