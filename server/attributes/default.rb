281cbd5b5ee35e806c948dd93bd8274feb1caa19
Daniel Santos <dsantos@cryptolab.net>
2014-12-05 01:42:41 +0000

    added sysctl

--- server/attributes/default.rb
+++ server/attributes/default.rb
@@ -3,7 +3,7 @@ default[:redis][:sysctl]['vm.swappiness']=0
 default[:redis][:sysctl]['net.ipv4.tcp_sack']=1
 default[:redis][:sysctl]['net.ipv4.tcp_timestamps']=1
 default[:redis][:sysctl]['net.ipv4.tcp_window_scaling']=1
-default[:redis][:sysctl]['net.ipv4.tcp_congestion_control']=htcp
+default[:redis][:sysctl]['net.ipv4.tcp_congestion_control']='htcp'
 default[:redis][:sysctl]['net.ipv4.tcp_syncookies']=1
 default[:redis][:sysctl]['net.ipv4.tcp_tw_recycle']=1
 
