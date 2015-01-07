3a05ae92350c16efe2a8ab6550374515fa418aba
Daniel Santos <dsantos@cryptolab.net>
2015-01-07 17:33:39 +0000

    polishing stuff

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -24,6 +24,12 @@ if node['docker']['opts']
   end
 
   service "docker" do
+    case node["platform"]
+    when "ubuntu"
+      if node["platform_version"].to_f >= 9.10
+        provider Chef::Provider::Service::Upstart
+      end
+    end
     action :restart
   end
 
