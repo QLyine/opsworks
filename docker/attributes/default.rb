26afe81fb09569dfdbf0c81692c590f0864e54ba
Daniel Santos <dsantos@cryptolab.net>
2015-01-07 18:03:20 +0000

    polishing stuff

--- docker/attributes/default.rb
+++ docker/attributes/default.rb
@@ -1,4 +1,4 @@
-if node[:opsworks][:instance][:instance_type] == 'm3.large' && !node[:opsworks][:instance][:hostname].start_with?('awsdb') 
+if node[:opsworks][:instance][:instance_type] == "m3.large" 
   default['docker']['opts'] = '-g /mnt/dockerdata'
 else
   default['docker']['opts'] = nil
