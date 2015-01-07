472adb5f3f1b21581eda712b3b237b1f34ca17d5
Daniel Santos <dsantos@cryptolab.net>
2015-01-07 17:23:39 +0000

    polishing stuff

---
+++ docker/attributes/default.rb
@@ -0,0 +1,5 @@
+if node[:opsworks][:instance][:instance_type] == 'm3.large' && !node[:opsworks][:instance][:hostname].start_with?('awsdb') 
+  default['docker']['opts'] = '-g /mnt/dockerdata'
+else
+  default['docker']['opts'] = nil
+end
--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -9,23 +9,23 @@ script "update_docker_repo" do
   EOH
 end  
 
-#if node['docker']['opts'] 
-#
-#  directory "/mnt/dockerdata" do
-#    owner 'root'
-#    group 'root'
-#    mode '700'
-#    action :create
-#  end
-#
-#  template '/etc/default/docker' do 
-#    source "docker.sysconfig.erb"
-#    mode "0644"
-#  end
-#
-#  service "docker" do
-#    action :restart
-#  end
-#
-#end
+if node['docker']['opts'] 
+
+  directory "/mnt/dockerdata" do
+    owner 'root'
+    group 'root'
+    mode '700'
+    action :create
+  end
+
+  template '/etc/default/docker' do 
+    source "docker.sysconfig.erb"
+    mode "0644"
+  end
+
+  service "docker" do
+    action :restart
+  end
+
+end
 
