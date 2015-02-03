bd4578a21d3298a6b5b996ff6459d62ff8c81811
Daniel Santos <dsantos@cryptolab.net>
2015-02-03 16:24:43 +0000

    LVM recipe

---
+++ server/recipes/swap2.rb
@@ -0,0 +1,40 @@
+swap_size_megs = (node['create-swap']['swap-size'] * 1024)
+
+if node['create-swap']['swap-device']
+
+  execute 'create swapfile' do
+    command <<-EOF.gsub(/^\s{4}/, '')
+      dd if=/dev/zero of=#{node['create-swap']['swap-location']} bs=1M count=#{swap_size_megs}
+      chmod 600 #{node['create-swap']['swap-location']}
+      mkswap #{node['create-swap']['swap-location']}
+    EOF
+
+    not_if { ::File.exists? node['create-swap']['swap-location'] }
+  end
+
+  mount 'none' do
+    action :enable
+    device node['create-swap']['swap-location']
+    fstype 'swap'
+  end
+
+else 
+
+  execute 'create swapdevice' do 
+    command <<-EOF.gsub(/^\s{4}/, '')
+      mswap -f #{node['create-swap']['swap-device']}
+    EOF
+  end
+
+  mount 'swap' do 
+    action :enable
+    device node['create-swap']['swap-device']
+    fstype swap
+    dump 0
+    pass 0
+  end
+end
+
+execute 'activate swap' do
+  command 'swapon -a'
+end
