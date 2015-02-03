10dff6da70a1d50242224f70ee638d0e636718eb
Daniel Santos <dsantos@cryptolab.net>
2015-02-03 15:42:11 +0000

    LVM recipe

---
+++ opslvm/recipes/default.rb
@@ -0,0 +1,20 @@
+include_recipe 'lvm::default'
+
+lvm_volume_group 'vg00' do
+  
+  if node[:opsworks][:instance][:instance_type] == "c3.xlarge"
+    physical_volumes ['/dev/xvdb', '/dev/xvdc']
+  end 
+
+  logical_volume 'swap' do
+    size        '8G'
+    filesystem  'swap'
+  end
+
+  logical_volume 'dockerdata' do
+    size        '72G'
+    filesystem  'ext4'
+    mount_point '/mnt'
+  end
+end
+
