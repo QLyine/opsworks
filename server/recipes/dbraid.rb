c4e27645654286c311f7ee8a3c2b9c9ba56c40ab
Daniel Santos <dsantos@cryptolab.net>
2015-02-25 16:06:42 +0000

    Added mdadm recipe

--- server/recipes/dbraid.rb
+++ server/recipes/dbraid.rb
@@ -1,9 +1,17 @@
-package "htop" do
+%w{mdadm}.each do |pkg|
+  package pkg do
     action :install
+  end
 end
 
-mdadm "/dev/md0" do
-  devices [ "/dev/xvdi", "/dev/xvdj", "/dev/xvdl", "/dev/xvdk" ]
-  level 0
-  action :create
+script "create_md0" do 
+  interpreter "bash" 
+  user "root" 
+  code <<-EOH 
+    yes | sudo mdadm --create /dev/md0 --level=0 -c256 --raid-devices=4 /dev/xvd[ijlk] ;
+    echo 'DEVICE /dev/xvdk /dev/xvdl /dev/xvdj /dev/xvdi' | sudo tee /etc/mdadm/mdadm.conf ;
+    mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf ;
+    update-initramfs -u ;
+    sudo blockdev --setra 65536 /dev/md0;
+  EOH
 end
