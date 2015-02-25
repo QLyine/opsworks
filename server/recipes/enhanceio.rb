cda7ce388319bc4ba3c0989441c3e15a95c92e3f
Daniel Santos <dsantos@cryptolab.net>
2015-02-25 16:22:58 +0000

    Added mdadm recipe

---
+++ server/recipes/enhanceio.rb
@@ -0,0 +1,26 @@
+reboot "now" do
+  action :nothing
+  reason "Cannot continue Chef run without a reboot."
+  delay_mins 1
+end
+
+
+%w{build-essential dkms lvm2 git}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+execute "install_eio" do 
+  command <<-EOH 
+    cd /root ;
+    git clone https://github.com/stec-inc/EnhanceIO.git ;
+    cd EnhanceIO/Driver/enhanceio/;
+    make && make install ;
+    cd ../../CLI/;
+    cp eio_cli /sbin/ ;
+    cp eio_cli.8 /usr/share/man/man8 ;
+  EOH
+  notifies :reboot_now, 'reboot[now]', :immediately
+end
+
