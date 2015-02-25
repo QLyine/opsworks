35f533510aa26d2f3cbffbe7ee001b4cc48cb830
Daniel Santos <dsantos@cryptolab.net>
2015-02-25 16:08:13 +0000

    Added mdadm recipe

--- server/recipes/dbraid.rb
+++ server/recipes/dbraid.rb
@@ -8,6 +8,7 @@ script "create_md0" do
   interpreter "bash" 
   user "root" 
   code <<-EOH 
+    umount /disco{1,2,3,4} ; 
     yes | sudo mdadm --create /dev/md0 --level=0 -c256 --raid-devices=4 /dev/xvd[ijlk] ;
     echo 'DEVICE /dev/xvdk /dev/xvdl /dev/xvdj /dev/xvdi' | sudo tee /etc/mdadm/mdadm.conf ;
     mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf ;
