a789c10e4b324511fecc4615a5264e8252577a2b
Daniel Santos <dsantos@cryptolab.net>
2015-02-26 11:58:26 +0000

    Added mdadm recipe

--- docker/attributes/default.rb
+++ docker/attributes/default.rb
@@ -3,7 +3,7 @@ it = node[:opsworks][:instance][:instance_type]
 # Default Doceker Opts
 default['docker']['opts'] = nil 
 
-unless node[:opsworks][:instance][:hostname].start_with?("awsdb") 
+unless node[:opsworks][:instance][:hostname].start_with?("awsdb") || node[:opsworks][:instance][:hostname].start_with?("analyticsdbmaster") 
   case it
   when "m3.large", "m3.xlarge", "c3.large", "c3.xlarge"
     default['docker']['opts'] = '-g /mnt/dockerdata'
