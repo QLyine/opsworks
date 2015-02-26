255521a32e67a0314aa0b7a589c8466f60275033
Daniel Santos <dsantos@cryptolab.net>
2015-02-26 10:28:11 +0000

    Added mdadm recipe

--- server/recipes/dbraid.rb
+++ server/recipes/dbraid.rb
@@ -9,8 +9,8 @@ mountLocation = "/srv/data"
 
 # Create data directory to mount RAID to
 directory mountLocation do
-  owner root
-  group root
+  owner 'root'
+  group 'root'
   mode 00755
   action :create
   not_if { FileTest.blockdev?(target) }
@@ -32,15 +32,3 @@ mount mountLocation do
   action :mount
 end
 
-#script "create_md0" do 
-#  interpreter "bash" 
-#  user "root" 
-#  code <<-EOH 
-#    umount /disco{1,2,3,4} ; 
-#    yes | sudo mdadm --create /dev/md0 --level=0 -c256 --raid-devices=4 /dev/xvd[ijlk] ;
-#    echo 'DEVICE /dev/xvdk /dev/xvdl /dev/xvdj /dev/xvdi' | sudo tee /etc/mdadm/mdadm.conf ;
-#    mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf ;
-#    update-initramfs -u ;
-#    sudo blockdev --setra 65536 /dev/md0;
-#  EOH
-#end
