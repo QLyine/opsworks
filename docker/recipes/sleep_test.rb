9426d83754a28b2dbfa659a3e6f36460c48a785d
Daniel Santos <dsantos@cryptolab.net>
2015-04-10 16:44:54 +0100

    Cenas

--- docker/recipes/sleep_test.rb
+++ docker/recipes/sleep_test.rb
@@ -1,6 +1,20 @@
-script "shutdown_#{node[:app][:dockers_aptstatsbe][:name]}_container" do
-  interpreter "bash"
-  user "root"
+#script "shutdown_#{node[:app][:dockers_aptstatsbe][:name]}_container" do
+#  interpreter "bash"
+#  user "root"
+#  code <<-EOH
+#    DOMAIN="#{node[:app][:domain]}";
+#    ARGS="#{node[:app][:dockers_aptstatsbe][:arg]}";
+#    NAME="#{node[:app][:dockers_aptstatsbe][:name]}";
+#    IMAGE="#{node[:app][:dockers_aptstatsbe][:image]}";
+#    DATA="#{node[:app][:dockers_aptstatsbe][:data]}";
+#    CONTAINERID=`docker ps | grep ${IMAGE} | awk '{print $1}'`
+#    [[ -n ${CONTAINERID} ]] && \
+#      docker exec -it ${CONTAINERID} sleep 1200
+#  EOH
+#end
+
+bash "sleep_#{node[:app][:dockers_aptstatsbe][:name]}_container" do 
+  timeout "3600"
   code <<-EOH
     DOMAIN="#{node[:app][:domain]}";
     ARGS="#{node[:app][:dockers_aptstatsbe][:arg]}";
@@ -12,4 +26,3 @@ script "shutdown_#{node[:app][:dockers_aptstatsbe][:name]}_container" do
       docker exec -it ${CONTAINERID} sleep 1200
   EOH
 end
-
