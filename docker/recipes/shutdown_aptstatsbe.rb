8a4882c2ecd1d2a24d2955f472246f35e1f633c7
Daniel Santos <dsantos@cryptolab.net>
2015-04-10 12:01:00 +0100

    Cenas

--- docker/recipes/shutdown_aptstatsbe.rb
+++ docker/recipes/shutdown_aptstatsbe.rb
@@ -8,7 +8,7 @@ script "shutdown_#{node[:app][:dockers_aptstatsbe][:name]}_container" do
     IMAGE="#{node[:app][:dockers_aptstatsbe][:image]}";
     DATA="#{node[:app][:dockers_aptstatsbe][:data]}";
     CONTAINERID=`docker ps | grep ${IMAGE} | awk '{print $1}'`
-    docker exec -it ${CONTAINERID} "sudo -u www-data php /srv/www/aptstats.aptoide.com/cli/cli_threads_pause.php --log"
+    docker exec -it ${CONTAINERID} sudo -u www-data php /srv/www/aptstats.aptoide.com/cli/cli_threads_pause.php --log
   EOH
 end
 
